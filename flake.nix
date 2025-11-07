995e04daac94e3b3f95ce7cb595ee6ab17ef018f
beeb5k <beeb5k6@gmail.com>
2025-11-07 12:33:54 +0530

    feat(nix): add flake.nix with Rust build and devShell

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      naersk,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naerskLib = pkgs.callPackage naersk { };
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        myPackage = naerskLib.buildPackage {
+          pname = cargoToml.package.name;
+          version = cargoToml.package.version;
+          edition = cargoToml.package.edition;
+          src = ./.;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustfmt
+            rustc
+            rust-analyzer
+            clippy
+            lld
+          ];
+        };
+
+        packages.default = myPackage;
+      }
+    );
+}
